c99e6284ec7b34c17d02de995ba4a183e4b16765
Farmayil001 <gasanoff8085@gmail.com>
2024-10-26 14:16:33 +0400

    #

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ const portfolioItems = [
 
 const portfolio = document.querySelector(".portfolio");
 
+
 portfolioItems.map((item, index) => {
 
     portfolio.innerHTML += `
@@ -48,10 +49,51 @@ portfolioItems.map((item, index) => {
 
 })
 
-const imgbox = document.querySelectorAll(".imgbox")
+const imgbox = document.querySelectorAll(".imgbox");
+const modal = document.querySelector(".modal-bg");
 
-imgbox.forEach(element => {
-    element.addEventListener("click", (e)=>{
-        
+imgbox.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        modal.classList.add("show");
+        modal.innerHTML = `
+            <div class="modal">
+            <div class="close-modal">
+                <img class="close" src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/close-icon.svg" alt="">
+            </div>
+            <div class="modal-row">
+                <div class="modal-content">
+                    <h2 class="modal-h2">Project Name</h2>
+                    <p class="modal-p">Lorem ipsum dolor sit amet consectetur.</p>
+                    <img src="${portfolioItems[index].img}"
+                        alt="">
+                    <p class="product-about">
+                        Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi
+                        sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!
+                    </p>
+                    <ul type="none">
+                        <li>
+                            <strong>Client:</strong>
+                            ${portfolioItems[index].title}
+                        </li>
+                        <li>
+                            <strong>Category:</strong>
+                            ${portfolioItems[index].about}
+                        </li>
+                    </ul>
+                    <button class="modal-btn close">&#10005 CLOSE PROJECT</button>
+                </div>
+            </div>
+        </div>
+        `
+
+        const close =()=>{
+            modal.classList.remove("show");
+        }
+
+        const modalClose = modal.querySelectorAll(".close");
+        modalClose.forEach(element=>{
+            element.addEventListener('click', ()=> close())
+        })        
     })
 });
